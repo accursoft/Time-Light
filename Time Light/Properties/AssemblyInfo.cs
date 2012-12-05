23a50e45f2e1c7acbbd5aac0d53128d230d12c1e
Gideon Sireling <code@accursoft.com>
2012-12-05 10:12:49 +0200

    assembly metadata

--- Time Light/Properties/AssemblyInfo.cs
+++ Time Light/Properties/AssemblyInfo.cs
@@ -2,11 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Time Light")]
-[assembly: AssemblyCompany ("Accursoft")]
+[assembly: AssemblyCompany("Accursoft")]
 [assembly: AssemblyProduct("Time Light")]
+[assembly: AssemblyDescription("Light-weight application for tracking detailed time expenditure over multiple projects")]
+[assembly: AssemblyCopyright("GPLv3")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("e6352f37-7296-478f-bf82-e29f169cb247")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
